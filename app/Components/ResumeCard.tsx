41e75a9a438074bda165af2b26c66d8b56d5e9c1
Josi <jsphtadesse@gmail.com>
2025-10-20 00:09:03 +0300

    Authentication

--- app/Components/ResumeCard.tsx
+++ app/Components/ResumeCard.tsx
@@ -17,16 +17,19 @@ const ResumeCard = ({resume :{id, companyName, jobTitle, feedback, imagePath}} :
                 <div className="flex-shrink-0">
                     <ScoreCircle score={feedback.overallScore}/>
                 </div>
-                <div className="gradient-border animate-in fade-in duration-1000">
-                    <div className="w-full h-full">
-                        <img
-                          src={imagePath}
-                          alt="resume"
-                          className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
-                        />
-                    </div>
+
+
+            </div>
+            <div className="gradient-border animate-in fade-in duration-1000">
+                <div className="w-full h-full">
+                    <img
+                        src={imagePath}
+                        alt="resume"
+                        className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+                    />
                 </div>
             </div>
+
         </Link>
     );
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import type { Route } from "./+types/home";
 import Navbar from "~/Components/Navbar";
 import {resumes} from "../../Constants";
 import ResumeCard from "~/Components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -12,14 +15,25 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const { auth } = usePuterStore(); // Added auth destructuring
+    const navigate = useNavigate();
+
+    // Extract "next" parameter safely
+
+
+    useEffect(() => {
+        if (!auth?.isAuthenticated) {
+            navigate("/auth?next=/");
+        }
+    }, [auth.isAuthenticated]);
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
-          <div className="page-heading">
+          <div className="page-heading py-16">
               <h1>Track Your Application & Resume Ratings </h1>
               <h2>Review Your submissions and check AI-powered feedback</h2>
           </div>
-      </section>
+
       {resumes.length > 0 && (
               <div className="resumes-section">
                   {resumes.map((resume) => (
@@ -28,6 +42,6 @@ export default function Home() {
               </div>
           )
       }
-
+      </section>
   </main>
 }
